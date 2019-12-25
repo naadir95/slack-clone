45eacc68b8ecc1ffd5b8c924bda59709b29b576c
Naadir Dawood <naadir@dev-macbook-naadir.Home>
2019-12-25 19:20:50 +0200

    Initialise postgres db

---
+++ models/user.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("user", {
+      username: {
+         type: DataTypes.STRING,
+         unique: true
+      },
+      email: {
+         type: DataTypes.STRING,
+         unique: true
+      },
+      password: {
+         type: DataTypes.STRING,
+      }
+    });
+  
+    User.associate =  (models) => {
+        User.belongsToMany(models.Team,{
+            through:'member',
+            foreignKey: 'UserId'
+        })
+    };
+  
+    return User;
+  };
